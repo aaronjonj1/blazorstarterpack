dd74c92110087528eec85dfca3ef592aec76b5e0
aaronjonj1 <ajjones3881@eagle.fgcu.edu>
2021-10-22 11:04:45 -0400

    cs class

---
+++ Client/Car.razor.cs
@@ -0,0 +1,56 @@
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace ConsoleApp3
+{
+
+    public class DoCar : ComponentBase
+    {
+        public string OutputMake { get; set; }
+
+        public string OutputModel { get; set; }
+
+        public string OutputColor { get; set; }
+
+        public string OutputYear { get; set; }
+
+        public string OutputPrice { get; set; }
+
+        string make;
+
+        string model;
+
+        string color;
+
+        int year;
+
+        string price;
+
+
+
+        private void Main()
+
+        {
+            DoCar Toyota = new DoCar();
+
+            Toyota.make = "Toyota";
+
+            Toyota.model = "Camry";
+
+            Toyota.color = "Gray";
+
+            Toyota.year = 2010;
+
+            Toyota.price = "$5,650";
+
+            OutputMake = Toyota.make.ToString();
+            OutputModel = Toyota.model.ToString();
+            OutputColor = Toyota.color.ToString();
+            OutputYear = Toyota.year.ToString();
+            OutputPrice = Toyota.price.ToString();
+
+
+
+        }
+    }
+}
