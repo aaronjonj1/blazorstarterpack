baf227fc43f61117f540b7af2c37806e6bb455ed
aaronjonj1 <ajjones3881@eagle.fgcu.edu>
2021-10-25 13:06:03 -0400

    back to older code

--- Client/Pages/Car.razor.cs
+++ Client/Pages/Car.razor.cs
@@ -1,86 +1,37 @@
 using Microsoft.AspNetCore.Components;
-
+using BlazorApp;
+using BlazorApp.Shared;
 
 namespace carProgram
 {
 
     public class DoCar : ComponentBase
     {
-        public string OutputMake;
-
-        public string OutputModel;
-
-        public string OutputColor;
-
-        public string OutputYear;
-
-        public string OutputPrice;
-
-
+        public string OutputMake { get; set; }
+        public string OutputModel { get; set; }
+        public string OutputColor { get; set; }
+        public string OutputYear { get; set; }
+        public string OutputPrice { get; set; }
         string make;
-
         string model;
-
         string color;
-
         int year;
-
         string price;
 
-
-
-
-
-
-
-        public void Camry()
+        public void Main()
 
         {
-            DoCar Camry = new DoCar();
-
-            Camry.make = "Toyota";
-
-            Camry.model = "Camry";
-
-            Camry.color = "Gray";
-
-            Camry.year = 2010;
-
-            Camry.price = "$5,650";
-
-            OutputMake = make.ToString();
-            OutputModel = model.ToString();
-            OutputColor = color.ToString();
-            OutputYear = year.ToString();
-            OutputPrice = price.ToString();
-      
-
-
-        }
-
-        public void MustangGT()
-
-        {
-            DoCar MustangGT = new DoCar();
-
-            MustangGT.make = "Ford";
-
-            MustangGT.model = "Mustang GT";
-
-            MustangGT.color = "Black";
-
-            MustangGT.year = 2018;
-
-            MustangGT.price = "$29,975";
-
-            OutputMake = make.ToString();
-            OutputModel = model.ToString();
-            OutputColor = color.ToString();
-            OutputYear = year.ToString();
-            OutputPrice = price.ToString();
-
-
-
+            DoCar Toyota = new DoCar();
+            Toyota.make = "Toyota";
+            Toyota.model = "Camry";
+            Toyota.color = "Gray";
+            Toyota.year = 2010;
+            Toyota.price = "$5,650";
+            OutputMake = Toyota.make.ToString();
+            OutputModel = Toyota.model.ToString();
+            OutputColor = Toyota.color.ToString();
+            OutputYear = Toyota.year.ToString();
+            OutputPrice = Toyota.price.ToString();
         }
     }
 }
