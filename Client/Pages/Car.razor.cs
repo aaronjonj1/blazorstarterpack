e6021e2eb1c61b3d50dc1ec33d5c4efb7e83769b
aaronjonj1 <ajjones3881@eagle.fgcu.edu>
2021-10-25 13:36:05 -0400

    fixer upper

--- Client/Pages/Car.razor.cs
+++ Client/Pages/Car.razor.cs
@@ -12,13 +12,17 @@ namespace carProgram
         public string OutputColor { get; set; }
         public string OutputYear { get; set; }
         public string OutputPrice { get; set; }
+        public string OutputMileage { get; set; }
         public string OutputURL { get; set; }
+        public string OutputNotes { get; set; }
         string make;
         string model;
         string color;
         int year;
         string price;
         string URL;
+        int mileage;
+        string notes;
 
         public void Main()
 
@@ -29,13 +33,17 @@ namespace carProgram
             Toyota.color = "Gray";
             Toyota.year = 2010;
             Toyota.price = "$5,650";
+            Toyota.mileage = 74561;
             Toyota.URL = "https://media.ed.edmunds-media.com/toyota/camry/2010/oem/2010_toyota_camry_sedan_le_fq_oem_1_500.jpg";
+            Toyota.notes = "";
             OutputMake = Toyota.make.ToString();
             OutputModel = Toyota.model.ToString();
             OutputColor = Toyota.color.ToString();
             OutputYear = Toyota.year.ToString();
             OutputPrice = Toyota.price.ToString();
+            OutputMileage = Toyota.mileage.ToString();
             OutputURL = Toyota.URL.ToString();
+            OutputNotes = Toyota.notes.ToString();
         }
     }
 }
