10c24fa663dec90289c113f9f3c95c6b8c55d167
aaronjonj1 <ajjones3881@eagle.fgcu.edu>
2021-10-25 19:53:48 -0400

    inheritance

---
+++ Client/Pages/Salary.razor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SalaryProgram
+{
+    public class Average
+    {
+        public float salary = 51909;
+    }
+    public class Programmer : Average
+    {
+        public float programmerSalary = 95640;
+    }
+    class TestInheritance
+    {
+        public static void Main(string[] args)
+        {
+            Job p1 = new Programmer();
+
+            Console.WriteLine("Average Salary: " + p1.salary);
+            Console.WriteLine("Programmer Salary: " + p1.programmerSalary);
+
+        }
+    }
+}
